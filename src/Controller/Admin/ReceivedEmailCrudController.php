dc50ddeffaba364c0f1772fc5551761aead58e42
Valentinas <plankton546@gmail.com>
2025-12-28 19:00:05 +0200

    Ability to view email HTML from admin panel

--- src/Controller/Admin/ReceivedEmailCrudController.php
+++ src/Controller/Admin/ReceivedEmailCrudController.php
@@ -10,6 +10,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class ReceivedEmailCrudController extends AbstractCrudController
 {
@@ -35,11 +37,34 @@ class ReceivedEmailCrudController extends AbstractCrudController
         ];
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setDefaultSort(['createdAt' => 'DESC']);
+    }
+
     public function configureActions(Actions $actions): Actions
     {
+        $viewHtml = Action::new('viewHtml', 'View HTML', 'fas fa-file-alt')
+            ->linkToUrl(function ($entity) {
+                return $this->generateUrl('admin_received_email_view_html', [
+                    'id' => $entity->getId(),
+                ]);
+            })
+            ->setHtmlAttributes(['target' => '_blank']);
+
         return $actions
             ->disable(Action::NEW, Action::EDIT)
+            ->add(Crud::PAGE_INDEX, $viewHtml)
             ->add(Crud::PAGE_INDEX, Action::DETAIL)
         ;
+    }
+
+    #[Route('/admin/received-email/{id}/view-html', name: 'admin_received_email_view_html')]
+    public function viewHtml(ReceivedEmail $email): Response
+    {
+        return $this->render('admin/received_email/view_html.html.twig', [
+            'html' => $email->getHtml()
+        ]);
     }
 }
